0f3327d597a51f06c8e21de2ddc8e090853e348a
AnhTrung732 <93305749+AnhTrung732@users.noreply.github.com>
2023-03-28 23:40:28 +0700

    Add Demo

--- app/src/main/java/com/example/mobileyolox/api/ApiService.java
+++ app/src/main/java/com/example/mobileyolox/api/ApiService.java
@@ -3,13 +3,18 @@ package com.example.mobileyolox.api;
 import static com.example.mobileyolox.ScanQrApiActivity.SCAN_DOMAIN;
 
 import com.example.mobileyolox.model.APIOutput;
+import com.example.mobileyolox.model.TestAPI;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 
+import java.util.concurrent.TimeUnit;
+
 import okhttp3.MultipartBody;
+import okhttp3.OkHttpClient;
 import retrofit2.Call;
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
+import retrofit2.http.GET;
 import retrofit2.http.Multipart;
 import retrofit2.http.POST;
 import retrofit2.http.Part;
@@ -20,16 +25,27 @@ public interface ApiService {
 
     public static final String DOMAIN = SCAN_DOMAIN;
 
+    OkHttpClient okHttpClient = new OkHttpClient.Builder()
+            .connectTimeout(10, TimeUnit.SECONDS) // Increase the timeout to 60 seconds
+            .readTimeout(10, TimeUnit.SECONDS)
+            .build();
+
     Gson gson = new GsonBuilder().setDateFormat("yyyy MM dd HH:mm:ss").create();
 
     ApiService apiService = new Retrofit.Builder()
             .baseUrl(DOMAIN)
+            .client(okHttpClient)
             .addConverterFactory(GsonConverterFactory.create(gson))
             .build()
             .create(ApiService.class);
 
+    @GET("/")
+    Call<TestAPI> testResponse();
 
     @Multipart
     @POST("detect")
     Call<APIOutput> sendImageFile(@Part MultipartBody.Part file);
+
+
+
 }
---
+++ app/src/main/java/com/example/mobileyolox/model/TestAPI.java
@@ -0,0 +1,17 @@
+package com.example.mobileyolox.model;
+
+public class TestAPI {
+    String response;
+
+    public TestAPI(String response) {
+        this.response = response;
+    }
+
+    public String getResponse() {
+        return response;
+    }
+
+    public void setResponse(String response) {
+        this.response = response;
+    }
+}
